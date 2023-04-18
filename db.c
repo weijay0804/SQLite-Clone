ff208c29b8ac8c80d5ff402ad601c95049454bd2
weijay <weijay0804@gmail.com>
2023-04-19 01:45:07 +0800

    feat: add Part 4

--- db.c
+++ db.c
@@ -23,6 +23,8 @@ typedef enum
     PREPARE_SUCCESS,
     PREPARE_UNRECOGNIZED_STATEMENT,
     PREPARE_SYNTAX_ERROR,
+    PREPARE_STRING_TOO_LONG,
+    PREPARE_NEGATIV_ID,
 } PrepareResult;
 
 typedef enum
@@ -51,8 +53,8 @@ typedef struct
 {
 
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 // 先計算 Row 結構中每個屬性的大小
@@ -158,30 +160,59 @@ MetaCommandResult do_meta_command(InputBuffer *input_buffer)
     }
 }
 
+PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement)
+{
+
+    statement->type = STATEMENT_INSERT;
+
+    // 取得 id username email 資料
+    // 取得 insert 關鍵字
+    char *keyword = strtok(input_buffer->buffer, " ");
+    char *id_string = strtok(NULL, " ");
+    char *username = strtok(NULL, " ");
+    char *email = strtok(NULL, " ");
+
+    if (id_string == NULL || username == NULL || email == NULL)
+    {
+
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    // 將 id 字串轉換成 int
+    int id = atoi(id_string);
+
+    if (id < 0)
+    {
+        return PREPARE_NEGATIV_ID;
+    }
+
+    // 檢查 username 和 email 的長度
+    if (strlen(username) > COLUMN_USERNAME_SIZE)
+    {
+
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    if (strlen(email) > COLUMN_EMAIL_SIZE)
+    {
+
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+}
+
 PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
 {
 
     // 能夠支援 insert X X X 的語法
     if (strncmp(input_buffer->buffer, "insert", 6) == 0)
     {
-
-        statement->type = STATEMENT_INSERT;
-
-        // 取得 insert 1 username email 語句當中的 (1, username, email)
-        // 並儲存至 statement -> row_to_insert 結構中
-        int args_assigned = sscanf(
-            input_buffer->buffer, "insert %d %s %s",
-            &(statement->row_to_insert.id),
-            statement->row_to_insert.username,
-            statement->row_to_insert.email);
-
-        // 錯誤處理
-        if (args_assigned < 3)
-        {
-            return PREPARE_SYNTAX_ERROR;
-        }
-
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     }
 
     if (strcmp(input_buffer->buffer, "select") == 0)
@@ -362,6 +393,14 @@ int main(int argc, char *argv[])
 
         case (PREPARE_SUCCESS):
             break;
+
+        case (PREPARE_STRING_TOO_LONG):
+            printf("String is too long.\n");
+            continue;
+
+        case (PREPARE_NEGATIV_ID):
+            printf("ID must be positive.\n");
+            continue;
 
         case (PREPARE_SYNTAX_ERROR):
             printf("指令錯誤 無法解析指令\n");
